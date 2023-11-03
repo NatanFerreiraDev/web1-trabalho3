f3bc1259ff6a1ba720408541006b947953839953
Natan da Silveira Ferreira <142810235+NatanFerreiraDev@users.noreply.github.com>
2023-11-03 20:20:36 -0300

    Add files via upload

---
+++ funcoesFront.js
@@ -0,0 +1,180 @@
+import { getTabelaPrice,
+    calcularCoeficienteFinanciamento,
+     calcularTaxaDeJuros,
+     calcularFatorAplicado,
+     calcularValorFuturo,
+     calcularPrecoAPrazo,
+     calcularPMT,
+    getValorCorrigido,
+    calcularValorAVoltar,
+    converterJurosMensalParaAnual } from "./main.js";
+    
+
+
+function handleButtonClick(){
+
+    let tableElement,numeroParcelas,  juros,valorFinanciado,valorFinal,mesesAVoltar,temEntrada, imprimir;
+
+    tableElement = document.querySelector("#table-content");
+    let box1Element = document.querySelector("#left-box");
+    let box2Element = document.querySelector("#right-box");
+
+    $("#submitButton").click(function (event) {
+        numeroParcelas = document.querySelector("#parc").value;
+        juros = document.querySelector("#itax").value;
+        valorFinanciado = document.querySelector("#ipv").value;
+        valorFinal = document.querySelector("#ipp").value;
+        mesesAVoltar = document.querySelector("#ipb").value;
+        temEntrada = document.querySelector("#idp").checked;   
+        imprimir = document.querySelector("#ipr").checked;   
+
+        var errorMessage = "";
+        if (juros == 0 && valorFinal == 0) {
+            errorMessage +=
+            "<p>Taxa de juros e valor final não podem ser ambos nulos.</p>";
+        }
+        if (juros == 0 && $("#ipv").val() == 0) {
+            errorMessage +=
+            "<p>Taxa de juros e valor financiado não podem ser ambos nulos.</p>";
+        }
+        if ($("#ipv").val() == 0 && $("#ipp").val() == 0) {
+            errorMessage +=
+            "<p>Valor financiado e valor final não podem ser ambos nulos.</p>";
+        }
+        if (errorMessage != "") {
+            $("#errorMessage").html(errorMessage);
+            $("#errorMessage").show();
+            $("#successMessage").hide();
+
+        } else {
+            $("#successMessage").show();
+            $("#errorMessage").hide();
+
+            let tabelaPrice, valorCorrigido, coeficienteFinanciamento;
+
+
+            if(valorFinal == 0){
+                coeficienteFinanciamento = calcularCoeficienteFinanciamento(juros, numeroParcelas);
+                valorFinal = calcularValorFuturo(coeficienteFinanciamento,juros,valorFinanciado,numeroParcelas,temEntrada);
+            }
+            var minhaDiv = document.getElementById("div-box-nt");
+            minhaDiv.style.display = "none";
+            tabelaPrice = getTabelaPrice(valorFinanciado,valorFinal,numeroParcelas,juros,temEntrada);
+
+            ExibeTabelaPrice(tabelaPrice, tableElement);
+            valorCorrigido = getValorCorrigido(tabelaPrice,numeroParcelas,mesesAVoltar);
+
+            ExibeBox1(valorFinanciado,valorFinal,numeroParcelas,juros,temEntrada, mesesAVoltar,box1Element);
+            ExibeBox2(valorFinanciado,valorFinal,numeroParcelas,juros,temEntrada, valorCorrigido, box2Element);
+
+
+        }
+
+            
+    });
+}
+
+
+function ExibeTabelaPrice(tabelaPrice, tableDocumentElement){
+
+    let table = "";
+ 
+   for(let i = 0; i < tabelaPrice.length; i++){
+       if(i == 0){
+           table += "<thead><tr>";
+           tabelaPrice[i].forEach(function(element){
+               table += `<th> ${element} </th>`;
+                });
+           table += "</tr></thead>";
+       }else{
+           table += "<tr>";
+           tabelaPrice[i].forEach(function (element){
+               table += `<td> ${element} </td>`;
+           });
+           table += "</tr>";
+       }
+   }
+
+   tableDocumentElement.innerHTML = table;
+   
+}
+
+
+function ExibeBox1(precoAVista,precoAPrazo,numParcelas,taxaDeJuros,temEntrada,mesesAVoltar,divElement){
+    let valorParcelas = 0; 
+    let jurosReal = 0;
+
+    jurosReal = calcularTaxaDeJuros(precoAVista,precoAPrazo,numParcelas,temEntrada) * 100;
+    let coeficienteFinanciamento = calcularCoeficienteFinanciamento(jurosReal,numParcelas);
+
+    jurosReal = jurosReal.toFixed(4);
+
+    precoAPrazo = (precoAPrazo > 0) ? precoAPrazo : precoAVista * calcularCoeficienteFinanciamento;
+
+    let jurosTotal = 0, totalPago = 0, amortizacaoTotal = 0, saldoDevedorTotal = precoAVista;
+ 
+    let pmt = calcularPMT(precoAVista,coeficienteFinanciamento).toFixed(2);
+
+     valorParcelas = (precoAPrazo / numParcelas).toFixed(2);
+
+    let jurosUsado = taxaDeJuros > 0? taxaDeJuros : jurosReal;
+    jurosUsado = jurosUsado;
+
+    divElement.innerHTML = `<p>Parcelamento: ${numParcelas} </p>
+    <p>Taxa: ${taxaDeJuros}% Ao Mês (${converterJurosMensalParaAnual(taxaDeJuros)}% Ao Ano) </p>
+    <p>Valor Financiado: $ ${precoAVista} </p>
+    <p>Valor Final: $ ${precoAPrazo}</p>
+    <p>Meses a Voltar(Adiantados) ${mesesAVoltar} </p>
+    <p>Valor a voltar(Adiantamento da dívida) $ ${calcularValorAVoltar(pmt,numParcelas,mesesAVoltar).toFixed(2)} </p>
+    <p>Entrada: ${temEntrada ? "Sim" : "Não"} </p>`;
+
+}
+
+function ExibeBox2(precoAVista,precoAPrazo,numParcelas,taxaDeJuros,temEntrada,valorCorrigido,divElement){
+
+    let text = "";
+
+    let valorParcelas = 0; 
+    let jurosReal = 0;
+
+    let quantidadeParcelas = (temEntrada) ? (numParcelas - 1) : numParcelas;
+
+
+    jurosReal = calcularTaxaDeJuros(precoAVista,precoAPrazo,numParcelas,temEntrada) * 100;
+    let coeficienteFinanciamento = calcularCoeficienteFinanciamento(jurosReal,numParcelas);
+
+
+    jurosReal = jurosReal.toFixed(4);
+
+    precoAPrazo = (precoAPrazo > 0) ? precoAPrazo : precoAVista * calcularCoeficienteFinanciamento;
+
+    let jurosTotal = 0, totalPago = 0, amortizacaoTotal = 0, saldoDevedorTotal = precoAVista;
+ 
+    let pmt = calcularPMT(precoAVista,coeficienteFinanciamento).toFixed(2);
+ 
+     valorParcelas = (precoAPrazo / numParcelas).toFixed(2);
+
+    let jurosUsado = taxaDeJuros > 0? taxaDeJuros : jurosReal;
+    jurosUsado = jurosUsado;
+
+    let juros = jurosUsado, amortizacao = 0, saldo = 0, saldoDevedor = precoAVista;
+
+
+    let jurosEmbutido = ((precoAPrazo - precoAVista) / precoAVista) * 100;
+    jurosEmbutido = jurosEmbutido.toFixed(2);
+    let desconto = ((precoAPrazo - precoAVista) / precoAPrazo) * 100;
+    desconto = desconto.toFixed(2);
+
+    divElement.innerHTML = `
+    <p> Prestação: $ ${pmt}</p>
+    <p> Taxa Real:  ${jurosReal}%</p>
+    <p> Coeficiente de Financiamento: ${coeficienteFinanciamento} </p>
+    <p>Fator Aplicado: ${calcularFatorAplicado(temEntrada,numParcelas,coeficienteFinanciamento,taxaDeJuros)}</p>
+    <p> Valor Corrigido: $ ${valorCorrigido} </p>
+    <p> Juros Embutido: ${jurosEmbutido}% </p>
+    <p> Desconto:  ${desconto}% </p>
+    `;
+}
+
+
+handleButtonClick();
---
+++ main.js
@@ -0,0 +1,249 @@
+/*
+Sumario do Pdf:
+
+v = valor de empréstimo
+x = preço à prazo
+y = preço à vista
+p = número de prestações
+R = valor de cada prestação
+A = Valor presente = valor a vista [fator *= 1 + t][fator, x * fator]
+valor  futuro = x(inverso / dividir  (fator =/ 1+ t))CF = Coeficiente de financiamento
+t = taxa(porcentagem)
+k = fator aplicado
+
+
+Valor a voltar: Adiantamento(pagar) 
+
+Valor final: Soma das parcelas
+
+
+Multiplicar juros por saldo devedor pra ter o primo juros
+    -> Juros[0] = juros do Usuario
+    -> Os demais = Juros * saldo
+     
+Se tem entrada, primeira prestacao = 0; amortização = 0; juros = juros; amortização = preco a vista, 
+
+Se tiver taxa a juros e valor a vista -> Calular o valor a prazo
+Se tiver valor a vista e a prazo -> Calcular taxa de juros commetodo de newton
+
+TODO: Usar a taxa de juros do usuario
+
+TODO: modularizar codigo pra pegar algumas informações individualmente
+
+TODO: transformar a tabela price em um array de arrays
+
+// ao usar navbar escuro, colocar navbar-dark dentro da classe e style = "background:<cor>"
+
+
+// CF * y = x
+// TODO: Usar visibilidade pra alternar entre formulario e Tabela
+
+*/
+
+// desconto racional por dentro | ou desconto por dentro
+function fe(ehPrimeiraTaxa, taxaJuros){
+    return  (ehPrimeiraTaxa == true)?  1 + taxaJuros : 1;
+}
+
+export function calcularCoeficienteFinanciamento(taxaJuros, quantidadeParcelas){
+    let taxaCorrigida = taxaJuros / 100;
+    return taxaCorrigida / (1 - Math.pow(1 + taxaCorrigida, -quantidadeParcelas) );
+}
+
+export function calcularValorPresente(coeficienteFinanciamento, taxaJuros, precoAPrazo,parcelas,ehPrimeiraTaxa){
+    let f = fe(ehPrimeiraTaxa, taxaJuros);
+
+    return (precoAPrazo / parcelas) * (f / coeficienteFinanciamento);
+}
+
+/*
+    Calcula o Valor Final(Preço à prazo) quando não fornecido(Precisa da taxa de Juros)
+*/
+export function calcularValorFuturo(coeficienteFinanciamento, taxaJuros, precoAVista,parcelas,temEntrada){
+
+    let resultado = precoAVista / calcularFatorAplicado(temEntrada,parcelas,coeficienteFinanciamento,taxaJuros);
+
+    return Number(resultado.toFixed(2));
+}
+
+ export function calcularFatorAplicado(temEntrada, numParcelas,coeficienteFinanciamento, taxaJuros){
+    let f = fe(temEntrada, taxaJuros);
+
+    return f/(numParcelas * coeficienteFinanciamento);
+}
+
+export function converterJurosMensalParaAnual(juros){
+    juros /= 100;
+    let resultado = (Math.pow(1 + juros, 12) - 1) * 100;
+    return Number(resultado.toFixed(2));
+}
+
+// Função para calcular a taxa de juros usando o Método de Newton
+export function calcularTaxaDeJuros(precoAVista, precoAPrazo, numParcelas, temEntrada) {
+    const tolerancia = 0.0001;  
+    let taxaDeJuros = 0.1; // Palpite inicial
+    let taxaDeJurosAnterior = 0.0;
+
+
+    let funcao = 0; let derivada = 0;
+    let iteracao = 0;
+
+    
+    while(Math.abs(taxaDeJurosAnterior - taxaDeJuros ) >= tolerancia){
+        
+        taxaDeJurosAnterior = taxaDeJuros;
+        funcao = calcularValorFuncao(precoAPrazo,taxaDeJuros,precoAVista,temEntrada,numParcelas);
+
+        derivada = calcularValorDerivadaFuncao(precoAPrazo,taxaDeJuros,precoAVista,temEntrada,numParcelas);
+
+        taxaDeJuros = taxaDeJuros - (funcao / derivada);
+
+        iteracao++;
+    }
+
+   
+    return taxaDeJuros;
+}
+
+export function getValorCorrigido(tabelaPrice,numeroParcelas,mesesAVoltar){
+    mesesAVoltar = Number(mesesAVoltar);
+    if(mesesAVoltar == 0 || mesesAVoltar >= numeroParcelas){
+        return 0;
+    }
+    else{
+        let tamanho = tabelaPrice.length - 2;
+        return tabelaPrice[tamanho - mesesAVoltar ][4];
+    }
+}
+
+export function calcularValorAVoltar(pmt, numeroParcelas,mesesAVoltar){
+    if( Number(mesesAVoltar) > Number(numeroParcelas)){
+        return 0;
+    }
+    else{
+        return pmt * mesesAVoltar;
+    }
+}
+
+function calcularValorFuncao(precoAPrazo,taxaDeJuros,precoAVista, temEntrada, numParcelas){
+let a = 0; let b = 0; let c = 0;
+    if(temEntrada){
+        a = Math.pow(1+taxaDeJuros,numParcelas-2);
+        b = Math.pow(1 + taxaDeJuros, numParcelas - 1);
+        c = Math.pow(1 + taxaDeJuros, numParcelas);
+
+        return (precoAVista * taxaDeJuros * b) - (precoAPrazo/numParcelas * (c - 1));
+       
+    }
+    else{
+        a = Math.pow(1 + taxaDeJuros, -numParcelas);
+        b = Math.pow(1 + taxaDeJuros, -numParcelas - 1 );
+
+        return (precoAVista * taxaDeJuros) - ( (precoAPrazo / numParcelas) * (1 - a) ); 
+    }
+}
+
+function calcularValorDerivadaFuncao(precoAPrazo,taxaDeJuros,precoAVista, temEntrada, numParcelas){
+    let a = 0; let b = 0;
+    // let c = 0;
+        if(temEntrada){
+            a = Math.pow(1+taxaDeJuros,numParcelas-2);
+            b = Math.pow(1 + taxaDeJuros, numParcelas - 1);
+           // c = Math.pow(1 + taxaDeJuros, numParcelas);
+    
+            return precoAVista * (b + (taxaDeJuros * a * (numParcelas - 1) ) ) - (precoAPrazo * b);
+           
+        }
+        else{
+            a = Math.pow(1 + taxaDeJuros, -numParcelas);
+            b = Math.pow(1 + taxaDeJuros, -numParcelas - 1 );
+    
+            return precoAVista - (precoAPrazo * b); 
+        }
+}
+    
+export function calcularPrecoAPrazo(precoAVista, taxaDeJuros, numParcelas ){
+    // let precoAPrazo = 0;
+
+    // for(let i =0; i < numParcelas; i++){
+    //     precoAPrazo += (precoAVista / numParcelas) * taxaDeJuros;
+    // }
+
+    // return precoAPrazo;
+    return precoAVista * (1 + taxaDeJuros)**numParcelas + taxaDeJuros;
+}
+
+export function calcularPMT(precoAVista,coeficienteFinanciamento){
+    return precoAVista * coeficienteFinanciamento;
+}
+
+// TODO Tirar Variavel Valor Parcela, uar PMT
+
+export function getTabelaPrice(precoAVista,precoAPrazo,numParcelas,taxaDeJuros,temEntrada){
+
+    let jurosReal = 0;
+
+    let quantidadeParcelas = (temEntrada) ? (numParcelas - 1) : numParcelas;
+
+
+    jurosReal = calcularTaxaDeJuros(precoAVista,precoAPrazo,numParcelas,temEntrada) * 100;
+    let coeficienteFinanciamento = calcularCoeficienteFinanciamento(jurosReal,numParcelas);
+
+
+    jurosReal = jurosReal.toFixed(4);
+
+    precoAPrazo = (precoAPrazo > 0) ? precoAPrazo : precoAVista * coeficienteFinanciamento;
+
+    let jurosTotal = 0, totalPago = 0, amortizacaoTotal = 0, saldoDevedorTotal = precoAVista;
+ 
+    let pmt =calcularPMT(precoAVista,coeficienteFinanciamento).toFixed(2);
+
+
+    let jurosUsado = taxaDeJuros > 0? taxaDeJuros : jurosReal;
+   // jurosUsado = jurosUsado;
+
+    let tabelaPrice = [["Mês","Prestação", "Juros", "Amortizacao","Saldo Devedor"]];
+
+  
+
+    let juros = jurosUsado, amortizacao = 0, saldo = 0, saldoDevedor = precoAVista;
+
+
+
+    for(let i = 1; i <= quantidadeParcelas; i++){
+
+
+        // se for a primeira taxa, usar o juros la, senao, calcular
+        juros = (saldoDevedor * jurosUsado)/100;
+        juros = juros.toFixed(2); 
+
+        amortizacao = (pmt - juros).toFixed(2);
+
+        saldoDevedor -=  amortizacao;
+        saldoDevedor = saldoDevedor > 0 ? saldoDevedor : 0;
+        saldoDevedor = saldoDevedor.toFixed(2);
+
+        //saldoDevedorTotal = saldoDevedorTotal - amortizacao;
+     
+        tabelaPrice.push([i,pmt, juros, amortizacao,saldoDevedor]);
+
+        jurosTotal +=  Number(juros);
+        totalPago += Number(pmt);
+        amortizacaoTotal += Number(amortizacao);
+
+    }
+
+    totalPago = totalPago.toFixed(2);
+    jurosTotal = jurosTotal.toFixed(2);
+    amortizacaoTotal = amortizacaoTotal.toFixed(2);
+
+    tabelaPrice.push([`Total:`, `${totalPago}`,`${jurosTotal}`, `${amortizacaoTotal}`,`${saldoDevedor}`]);
+
+    return tabelaPrice;
+}
+
+
+
+
+
+
